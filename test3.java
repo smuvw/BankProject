f7e62b119b6fb67b9262ad2cb34ac77ec8689b82
smuvw <srinivasarao_muvva@yahoo.com>
2017-01-25 19:33:24 -0600

    Create test3.java

---
+++ test3.java
@@ -0,0 +1,13 @@
+package PKG1;
+
+public class test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//this is comment
+   System.out.println("this is test3");
+	}
+
+}
+
