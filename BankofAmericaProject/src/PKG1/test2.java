af230fa8709c35742b0734ae0db0d0a5eb203447
SMuvva <SMuvva@192.168.222.1>
2017-01-25 19:27:33 -0600

    test2- updated

--- BankofAmericaProject/src/PKG1/test2.java
+++ BankofAmericaProject/src/PKG1/test2.java
@@ -4,7 +4,9 @@ public class test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("this is test2");
+		
+		//this is comment
+   System.out.println("this is test2");
 	}
 
 }
